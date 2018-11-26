bc1bd8c54c40963dca6bf82192c1c2d84e202bf9
Brandon Teich <brandonteich123@gmail.com>
2018-11-26 06:29:40 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,18 +1,14 @@
 class BankAccount
   attr_reader :balance
-
   def initialize(balance)
     @balance = balance
   end
-
   def display_balance
     @balance
   end
-
   def deposit(amount)
     @balance += amount if amount > 0
   end
-
   def withdraw(amount)
     @balance -= amount if amount <= @balance
   end
